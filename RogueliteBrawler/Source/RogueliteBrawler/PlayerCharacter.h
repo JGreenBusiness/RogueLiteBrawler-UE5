6c0ea03d41ab37af2cc9f9f414c8845f2e5235c0
Justin Green <justingreen.business@gmail.com>
2024-01-11 19:10:03 +1100

    Addition (PlayerCharacter&Input) :
    Created a 3rd person PlayerCharacter class and set up all action inputs including a primary action that for now, spherecasts out then moves then  slerps the player towards the first actor's location it detects. Depending on where player is facing also depends on weather a cast occurs

---
+++ RogueliteBrawler/Source/RogueliteBrawler/PlayerCharacter.h
@@ -0,0 +1,106 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Logging/LogMacros.h"
+#include "PlayerCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
+
+UCLASS(config = Game)
+class APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	/** Primary Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* PrimaryAction;
+	
+	UPROPERTY(EditAnywhere, Category = "SphereTrace")
+	float SphereCastDistance = 5;
+		
+	UPROPERTY(EditAnywhere, Category = "SphereTrace")
+	float SphereCastRadius = 5;
+
+	TArray<AActor*> ActorsToIgnore;
+
+
+	bool Attacking = false;
+
+	FVector CurrentTargetLocation = FVector(0);
+
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	float AttackSpeed = 15;
+	
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	float AttackRadius = 30;
+
+
+	UPROPERTY(EditAnywhere, Category = "Attack")
+	float DuelThreshold = .5f;
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	/** Called for Primary input */
+	void Primary();
+	void PrimaryRelease() {};
+
+	FVector MoveTowards(FVector Current, FVector Target, float MaxDelta);
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// To add mapping context
+	virtual void BeginPlay();
+
+	virtual void Tick(float DeltaTime);
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const {return FollowCamera;}
+};
